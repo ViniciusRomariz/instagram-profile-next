d04bf656421e19dec5701db62945b7170cedcb4b
Vinicius Romariz <vinicius.romariz@al.infnet.edu.br>
2023-01-09 14:55:19 -0300

    Teste 1

--- src/components/ProfileHeader.tsx
+++ src/components/ProfileHeader.tsx
@@ -23,7 +23,7 @@ export function ProfileHeader({
     <div className="profile-header">
       <div className="row">
         <div className="avatar item">
-          <Avatar src={"../src/images/vicrom.jpg"} size={120} alt={`Foto de Vinicius`} />
+          <Avatar src={"../public/vicrom.jpg"} size={120} alt={`Foto de Vinicius`} />
         </div>
       </div>
       <div className="row">
